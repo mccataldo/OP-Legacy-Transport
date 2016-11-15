6301e3eba5970eb4cde6644352e863d3d163eb4a
mike cataldo <mikecataldo@Mikes-MacBook-Pro-4.local>
2016-11-15 14:06:22 -0700

    removed semicolons and comments

--- copyAudit.js
+++ copyAudit.js
@@ -4,7 +4,7 @@ function copyAudit() {
 			{
 				"observePointAuditClone": doAuditCopy()
 			}
-		);
+		)
 		chrome.storage.sync.get("observePointAuditClone", function(obj){
 			var output = JSON.parse(obj["observePointAuditClone"])[0]
 			console.log(output)
@@ -21,16 +21,16 @@ function doAuditCopy() {
 	    mySettings = new Array,
 			action = jQuery(".action_row"),
 			compliance = jQuery("#rule_sets")
-			ruleList = new Array;
+			ruleList = new Array
 
 	for (i = 0; i < action.length; i++) {
-	    var clonedobj = jQuery.extend({}, act);
-	    clonedobj.ActionType = jQuery(action[i]).find("select").find(":selected").val();
-	    clonedobj.PreventNav = jQuery(action[i]).find('input[type="checkbox"]')[0].checked;
-	    clonedobj.Value = jQuery(action[i]).find("input[placeholder='value']").val();
-	    clonedobj.Identifier = jQuery(action[i]).find("input[placeholder='identifier']").val();
-			clonedobj.RegexFilter = jQuery(action[i]).find("input[placeholder='regex filter']").val();
-	    actList.push(clonedobj);
+	    var clonedobj = jQuery.extend({}, act)
+	    clonedobj.ActionType = jQuery(action[i]).find("select").find(":selected").val()
+	    clonedobj.PreventNav = jQuery(action[i]).find('input[type="checkbox"]')[0].checked
+	    clonedobj.Value = jQuery(action[i]).find("input[placeholder='value']").val()
+	    clonedobj.Identifier = jQuery(action[i]).find("input[placeholder='identifier']").val()
+			clonedobj.RegexFilter = jQuery(action[i]).find("input[placeholder='regex filter']").val()
+	    actList.push(clonedobj)
 	}
 
 	for (j = 0; j<compliance[0].length; j++) {
@@ -40,43 +40,36 @@ function doAuditCopy() {
 		}
 	}
 
+	var clonedobj = jQuery.extend({}, act)
 
-
-	var clonedobj = jQuery.extend({}, act);
-
-	clonedobj.Name = jQuery("#domain_description").val();
-	clonedobj.StartingPages = jQuery("#domain_starting_urls").val();
-	clonedobj.PageLimit = jQuery("#domain_max_url").val();
-	clonedobj.Location = jQuery("#domain_location_id").val();
+	clonedobj.Name = jQuery("#domain_description").val()
+	clonedobj.StartingPages = jQuery("#domain_starting_urls").val()
+	clonedobj.PageLimit = jQuery("#domain_max_url").val()
+	clonedobj.Location = jQuery("#domain_location_id").val()
 	clonedobj.LocationText = jQuery("#domain_location_id")[0].value
 	clonedobj.LocationProxyUrl = jQuery("#domain_proxy")[0].value
-	clonedobj.UserAgent = jQuery("#domain_user_agent_id").val();
-	clonedobj.UserAgentText = jQuery("#domain_user_agent_id [selected='selected']").text();
-	clonedobj.Schedule = jQuery("#auditnow").val();
+	clonedobj.UserAgent = jQuery("#domain_user_agent_id").val()
+	clonedobj.UserAgentText = jQuery("#domain_user_agent_id [selected='selected']").text()
+	clonedobj.Schedule = jQuery("#auditnow").val()
 	clonedobj.ScheduleText = jQuery("#auditnow")[0].selectedOptions[0].innerText
-	clonedobj.ScheduleFrequency = jQuery("#scan_frequency").val();
-	clonedobj.ScheduleFrequencyText = jQuery("#scan_frequency [selected='selected']").text();
-	clonedobj.NextStartDate = jQuery("#next_scan_at").val();
-	clonedobj.Notifications = jQuery("#domain_recipients").val();
-	clonedobj.ActionSet = actList;
-	clonedobj.ClearCookies = jQuery("#domain_clear_cookies").prop("checked");
-	clonedobj.SilentMode = jQuery("#domain_silent_mode").prop("checked");
-	clonedobj.TemplateMode = jQuery("#domain_template_mode").prop("checked");
-	clonedobj.ReduceServerLoad = jQuery("#reduce_server_load").prop("checked");
-	clonedobj.LoadVideos = jQuery("#domain_load_videos").prop("checked");
-	clonedobj.BrowserOverride = jQuery("#domain_browser_width").val();
-	clonedobj.IncludeList = jQuery("#inclusions").val();
-	clonedobj.ExcludeList = jQuery("#exclusions").val();
-	clonedobj.UserSession = jQuery('input[name="login_action"]')[0].checked;
-	clonedobj.Compliance = jQuery("#rule_sets").val();
-	clonedobj.ComplianceText = ruleList;
-	//clonedobj.Frequency = jQuery("#uniform-flow_monitor_frequency_in_minutes select").val();
-	//clonedobj.FrequencyText = jQuery("#uniform-flow_monitor_frequency_in_minutes")[0].innerText
-	//clonedobj.SendAlerts = jQuery("#flow_send_alerts").prop("checked");
-	//clonedobj.SendReminders = jQuery("#flow_send_reminders").prop("checked");
+	clonedobj.ScheduleFrequency = jQuery("#scan_frequency").val()
+	clonedobj.ScheduleFrequencyText = jQuery("#scan_frequency [selected='selected']").text()
+	clonedobj.NextStartDate = jQuery("#next_scan_at").val()
+	clonedobj.Notifications = jQuery("#domain_recipients").val()
+	clonedobj.ActionSet = actList
+	clonedobj.ClearCookies = jQuery("#domain_clear_cookies").prop("checked")
+	clonedobj.SilentMode = jQuery("#domain_silent_mode").prop("checked")
+	clonedobj.TemplateMode = jQuery("#domain_template_mode").prop("checked")
+	clonedobj.ReduceServerLoad = jQuery("#reduce_server_load").prop("checked")
+	clonedobj.LoadVideos = jQuery("#domain_load_videos").prop("checked")
+	clonedobj.BrowserOverride = jQuery("#domain_browser_width").val()
+	clonedobj.IncludeList = jQuery("#inclusions").val()
+	clonedobj.ExcludeList = jQuery("#exclusions").val()
+	clonedobj.UserSession = jQuery('input[name="login_action"]')[0].checked
+	clonedobj.Compliance = jQuery("#rule_sets").val()
+	clonedobj.ComplianceText = ruleList
 
+	mySettings.push(clonedobj)
 
-	mySettings.push(clonedobj);
-
-	return JSON.stringify(mySettings);
+	return JSON.stringify(mySettings)
 }
